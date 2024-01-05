b65dee05a0ebd87d700723b669489799b9bfab42
Megan <megan.koh96@gmail.com>
2024-01-05 21:34:35 +0800

    Initial Commit

---
+++ src/main/java/com/ssf/day16workshop/Day16WorkshopApplication.java
@@ -0,0 +1,13 @@
+package com.ssf.day16workshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day16WorkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day16WorkshopApplication.class, args);
+	}
+
+}
