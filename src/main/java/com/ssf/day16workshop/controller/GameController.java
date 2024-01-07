e813ebe522a11de17e2cd40a01edb445faaaadd9
Megan <megan.koh96@gmail.com>
2024-01-07 12:46:28 +0800

    Initial Commit

--- src/main/java/com/ssf/day16workshop/controller/GameController.java
+++ src/main/java/com/ssf/day16workshop/controller/GameController.java
@@ -9,6 +9,8 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -53,6 +55,26 @@ public class GameController {
                     "Internal Server Error"), HttpStatus.INTERNAL_SERVER_ERROR);
         }
 
+    }
+
+    @GetMapping("/api/boardgame/{id}")
+    public ResponseEntity<Map<String, Object>> getGame(@PathVariable String id) {
+        try {
+            Game game = gameRepo.getGame(id);
+
+            if (game != null) {
+                Map<String, Object> responsePayload = new HashMap<>();
+                responsePayload.put("game", game);
+                return ResponseEntity.ok(responsePayload);
+            } else {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                        .body(Collections.singletonMap("error", "Board game not found"));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(Collections.singletonMap("error", "Internal Server Error"));
+        }
     }
 
 }
--- src/main/java/com/ssf/day16workshop/repo/GameRepo.java
+++ src/main/java/com/ssf/day16workshop/repo/GameRepo.java
@@ -1,5 +1,7 @@
 package com.ssf.day16workshop.repo;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -24,6 +26,21 @@ public class GameRepo {
         template.opsForHash().put(key, "ranking", Integer.toString(game.getRanking()));
         template.opsForHash().put(key, "usersRated", Integer.toString(game.getUsersRated()));
         template.opsForHash().put(key, "url", game.getUrl().toString());
+
+    }
+
+    public Game getGame(String id) {
+        String key = GAME_HASH_KEY + ":" + id;
+        Map<Object, Object> gameData = template.opsForHash().entries(key);
+        Game game = new Game();
+        game.setGid(Integer.parseInt(gameData.get("gid").toString()));
+        game.setName(gameData.get("name").toString());
+        game.setYear(Integer.parseInt(gameData.get("year").toString()));
+        game.setRanking(Integer.parseInt(gameData.get("ranking").toString()));
+        game.setUsersRated(Integer.parseInt(gameData.get("usersRated").toString()));
+        game.setUrl(gameData.get("url").toString());
+
+        return game;
 
     }
 
